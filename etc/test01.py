b0ffc691db415dfd6b5e812b8f23bdc2a7b07332
skku-arspraxia <jihn011258@g.skku.edu>
2022-06-15 17:34:43 +0900

    0615

---
+++ etc/test01.py
@@ -0,0 +1,11 @@
+x=5
+while x>=0:
+    if x%2==1:
+        print("My Score")
+    elif x-5<0:
+        print("is")
+    elif x-3>4:
+        print("good")
+    else:
+        print("Not Good")
+    x=x-1
